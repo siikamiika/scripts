da7882ce8d5bca0ac7ab707c4a53cbd06e8df6d3
siikamiika <siikamiika@users.noreply.github.com>
2014-08-17 06:17:51 +0300

    dualaudiofix: accept titleless tracks

--- dualaudiofix.lua
+++ dualaudiofix.lua
@@ -1,8 +1,8 @@
 ---------------------------------------------
--- (for mpv media player)                  --
--- attempt ignore english subtitle tracks  --
--- that are intended to be used with the   --
--- english audio track on dual audio anime --
+-- (for mpv media player)
+
+-- attempt to ignore english subtitle tracks that are intended to be used
+-- with the english audio track on dual audio anime
 ---------------------------------------------
 
 -- local sub_languages = mp.get_property_native("options/slang")
@@ -10,8 +10,7 @@ local tracklist = mp.get_property_native("track-list")
 for idx, track in pairs(tracklist) do
     if track["type"] == "sub" then
         if (track["lang"] == "eng" or track["lang"] == "en") then
-            local track_title = track["title"]:lower()
-            if not string.match(track_title, "sign") then
+            if (not track["title"] or not string.match(track["title"]:lower(), "sign")) then
                 good_track = track["id"]
                 break
             end

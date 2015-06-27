6fa0f0d193af65d47a7c6b635f25c44ce4a6d4f0
siikamiika <siikamiika@users.noreply.github.com>
2015-06-27 09:30:16 +0300

    m3u_titles.lua: more reliable parsing

--- mpv scripts/m3u_titles.lua
+++ mpv scripts/m3u_titles.lua
@@ -8,6 +8,10 @@ function string.ends(String,End)
    return End=='' or string.sub(String,-string.len(End))==End
 end
 
+function trim(s)
+    return s:match'^%s*(.*%S)' or ''
+end
+
 function readAll(file)
     local f = io.open(file, "rb")
     local content = f:read("*all")
@@ -20,6 +24,7 @@ function parse_titles(m3u_file)
     local file_content = readAll(m3u_file)
     local playlist_counter = 1
     for line in file_content:gmatch("[^\r\n]+") do
+        local line = trim(line)
         if string.starts(line, "#EXTINF") then
             local extinf_table = {}
             for extinf in line:gmatch("[^,]+") do
@@ -28,7 +33,7 @@ function parse_titles(m3u_file)
             table.remove(extinf_table, 1)
             local title = table.concat(extinf_table, "")
             table.insert(titles, playlist_counter, title)
-        elseif not string.starts(line, "#EXTM3U") then
+        elseif not ((line == "") or string.starts(line, "#")) then
             playlist_counter = playlist_counter + 1
         end
     end

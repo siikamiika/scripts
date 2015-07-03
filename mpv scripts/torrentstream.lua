f81b0aa843398ad8dcc958d85fc375c9e5c0fcd7
siikamiika <siikamiika@users.noreply.github.com>
2015-07-03 05:57:32 +0300

    torrentstream: fix magnet links in playlists
    mpv thinks magnet links in playlists are files and adds the absolute path
    in front of them when the playlist is opened as an absolute path. Fixed
    magnet detection by searching the entire string for "magnet:" and
    stripping the leading path from it.

--- mpv scripts/torrentstream.lua
+++ mpv scripts/torrentstream.lua
@@ -2,9 +2,9 @@ local utils = require 'mp.utils'
 
 HOST = "http://localhost:8888"
 
-function string.starts(String,Start)
-   return string.sub(String,1,string.len(Start))==Start
-end
+-- function string.starts(String,Start)
+--    return string.sub(String,1,string.len(Start))==Start
+-- end
 
 function string.ends(String,End)
    return End=='' or string.sub(String,-string.len(End))==End
@@ -12,11 +12,12 @@ end
 
 mp.add_hook("on_load", 50, function ()
     local fn = mp.get_property("stream-open-filename")
+    local magnet_idx = string.find(fn, "magnet:")
     local torrent = nil
     if string.ends(fn, ".torrent") then
         torrent = utils.join_path(utils.getcwd(), fn)
-    elseif string.starts(fn, "magnet:") then
-        torrent = fn
+    elseif magnet_idx then
+        torrent = string.sub(fn, magnet_idx)
     end
     if torrent then
         mp.set_property("stream-open-filename", HOST.."/stream_torrent/"..torrent)

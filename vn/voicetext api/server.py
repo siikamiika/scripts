a415fbe7273d4e4d7bad095f50b8104708d74838
siikamiika <siikamiika@users.noreply.github.com>
2016-05-25 18:33:12 +0300

    voicetext: fix CORS

--- vn/voicetext api/server.py
+++ vn/voicetext api/server.py
@@ -6,9 +6,18 @@ import sys
 
 class VoiceText(web.RequestHandler):
 
+	def set_default_headers(self):
+		self.set_header('Access-Control-Allow-Origin', '*')
+		self.set_header('Access-Control-Allow-Headers', 'x-requested-with, content-type')
+		self.set_header('Access-Control-Allow-Methods', 'POST, OPTIONS')
+
 	def post(self):
 		data = json.loads(self.request.body)
 		call(['speak.bat', data.encode(sys.getfilesystemencoding())])
+	
+	def options(self):
+		self.set_status(204)
+		self.finish()
 
 def get_app():
 	return web.Application([
